8988e41cac1971974022f44c82543af1c7d4e5e4
Nate Harms <harms.n@husky.neu.edu>
2019-05-16 11:05:57 -0400

    pre-branching commit

--- autotst/data/io.py
+++ autotst/data/io.py
@@ -66,7 +66,7 @@ class InputOutput():
     A class that allows users to read and write .ts and .kinetics files for validated TSs and Reactions
     """
 
-    def __init__(self, reaction=None, save_directory="."):
+    def __init__(self, reaction=None, rmg_reaction=None, save_directory="."):
         """
         Variables:
         - reaction (Reaction): the reaction of interest
@@ -75,7 +75,9 @@ class InputOutput():
         self.reaction = reaction
         if reaction:
             self.label = reaction.label
+            self.rmg_reaction = reaction.rmg_reaction
         else:
+            self.rmg_reaction = rmg_reaction
             self.label = None
         self.save_directory = save_directory
 
@@ -96,15 +98,60 @@ class InputOutput():
 
         return QMData().get_qmdata(file_path=file_path)
 
-    def get_ts_file_path(self, reaction):
+    def get_label(self, reaction):
+        """
+        A method to convert an RMGReaction to a reaction label used for writing files
+        """
+        if isinstance(reaction, Reaction):
+            return reaction.label
+        assert isinstance(reaction, RMGReaction), "Must provide either an RMGReaction or Reaction object"
+        label = ""
+        if reaction is None:
+            return None
+        for reactant in reaction.reactants:
+            if isinstance(reactant, RMGMolecule):
+                label += reactant.toSMILES()
+            elif isinstance(reactant, RMGSpecies):
+                if len(RMGSpecies.molecule) > 1:
+                    logging.warning("{} has multiple resonance structures. Selecting the first molecule for creating the file path")
+                    label += reactant.molecule[0].toSMILES()
+                else:
+                    label += reactant.molecule[0].toSMILES()
+            else:
+                logging.warning("No reactants provided...")
+                return label
+            label += "+"
+        label = label.strip("+")
+        label += "_"
+        
+        for product in reaction.products:
+            if isinstance(reactant, RMGMolecule):
+                label += product.toSMILES()
+            elif isinstance(product, RMGSpecies):
+                if len(RMGSpecies.molecule) > 1:
+                    logging.warning("{} has multiple resonance structures. Selecting the first molecule for creating the file path")
+                    label += product.molecule[0].toSMILES()
+                else:
+                    label += product.molecule[0].toSMILES()
+            else:
+                logging.warning("No product provided...")
+                return None
+        return label
+        
 
-        return os.path.join(self.save_directory, reaction.label + ".ts")
+    def get_ts_file_path(self, save_directory=".", label=None):
+        
+        assert label, "Please provide a reaction label for saving information"
 
-    def get_kinetics_file_path(self, reaction):
+        return os.path.join(save_directory, label + ".ts")
 
-        return os.path.join(self.save_directory, reaction.label + ".kinetics")
+    def get_kinetics_file_path(self, save_directory=".", label=None):
+        
+        assert label, "Please provide a reaction label for saving information"
 
-    def save_ts(self, reaction, method, qmData):
+        return os.path.join(save_directory, label + ".kinetics")
+
+    def save_ts(self, reaction, qmData, method="m062x", save_directory="."):
         """
         A method to save the .ts data to a file
 
@@ -114,28 +161,37 @@ class InputOutput():
         - qmData (QMData): the data that you want to save
         """
         file_path = self.get_ts_file_path(reaction)
+        if not os.path.exists(save_directory):
+            try: 
+                os.makedirs(save_directory)
+            except: 
+                logging.info("Writing save directory")
         logging.info("Saving TS result file {}".format(file_path))
         with open(file_path, 'w') as resultFile:
             resultFile.write('rxnLabel = "{0!s}"\n'.format(reaction.label))
             resultFile.write('method = "{0!s}"\n'.format(method))
             resultFile.write("qmData = {0!r}\n".format(qmData))
 
-    def save_kinetics(self, method, reaction):
+    def save_kinetics(self, reaction, method="m062x", save_directory="."):
         """
-        A method to save the .ts data to a file
+        A method to save the .kinetics data to a file
 
         Variables:
-        - reaction (Reaction): the reaction of interest
+        - reaction (Reaction): the reaction of interest. Either an RMGReaction or an AutoTST Reaction.
         - method (str): the computational methods that were used
         """
-        filePath = self.get_kinetics_file_path(reaction)
+        if not os.path.exists(save_directory):
+            try: 
+                os.makedirs(save_directory)
+            except: 
+                logging.info("Writing save directory")
+        label = self.get_label(reaction)
+        filePath = self.get_kinetics_file_path(save_directory=save_directory, label=label)
         logging.info("Saving kinetics data file {}".format(filePath))
         with open(filePath, 'w') as resultFile:
-
             if isinstance(reaction, rmgpy.reaction.Reaction):
                 assert reaction.kinetics, "No kinetics calclated for this reaction..."
                 resultFile.write('method = "{0!s}"\n'.format(method))
-
                 resultFile.write('reaction = {0!r}\n'.format(reaction))
             elif isinstance(reaction, autotst.reaction.Reaction):
                 assert reaction.rmg_reaction.kinetics, "No kinetics calclated for this reaction..."
@@ -143,32 +199,17 @@ class InputOutput():
                 resultFile.write(
                     'reaction = {0!r}\n'.format(reaction.rmg_reaction))
 
-    def read_ts_file(self, reaction):
+    def read_ts_file(self, path):
         """
         Load the specified transition state data file and return the dictionary of its contents.
 
         Variables:
-        - reaction (Reaction): the reaction of interest
+        - path (str): the file that you want to read in. It should end with ".kinetics"
 
         Returns:
         - local_context (dict): the content of the .ts file. Will return None if there is an error
         """
 
-        r, p = reaction.label.split("_")
-        reacts = r.split("+")
-        prods = p.split("+")
-
-        file_names = get_possible_names(reacts, prods)
-
-        got_file = False
-        for file_name in file_names:
-            ts_file = os.path.join(self.save_directory, file_name + ".ts")
-            if os.path.exists(ts_file):
-                got_file = True
-                path = ts_file
-
-        if not got_file:
-            return None
         try:
             with open(path) as resultFile:
                 logging.info('Reading existing ts file {0}'.format(path))
@@ -203,32 +244,17 @@ class InputOutput():
             return None
         return local_context
 
-    def read_kinetics_file(self, reaction):
+    def read_kinetics_file(self, path):
         """
         Load the specified kinetic data file and return the dictionary of its contents.
 
         Variables:
-        - reaction (Reaction): the reaction of interest
+        - path (str): the file that you want to read in. It should end with ".kinetics"
 
         Returns:
         - local_context (dict): the content of the .kinetics file. Will return None if there is an error
         """
-        r, p = reaction.label.split("_")
-        reacts = r.split("+")
-        prods = p.split("+")
 
-        file_names = get_possible_names(reacts, prods)
-
-        got_file = False
-        for file_name in file_names:
-            ts_file = os.path.join(self.save_directory,
-                                   file_name + ".kinetics")
-            if os.path.exists(ts_file):
-                got_file = True
-                path = ts_file
-
-        if not got_file:
-            return None
         try:
             with open(path) as resultFile:
                 logging.info('Reading existing kinetics file {0}'.format(path))
