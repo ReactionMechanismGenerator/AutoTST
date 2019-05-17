673881e7c4342b4e2222e0f2f4c4a33975887c40
davidfarinajr <farina.d@husky.neu.edu>
2019-05-17 16:48:22 -0400

    added a second optimization with looser cutoffs for calculate_species using Gaussian calculator

--- autotst/job/job.py
+++ autotst/job/job.py
@@ -151,6 +151,138 @@ class Job():
 
         return label
 
+    def calculate_conformer(self, conformer, calculator):
+        calc = calculator.get_conformer_calc(conformer=conformer)
+
+        label = self.submit_conformer(conformer, calc, "general")
+        currently_running.append(label)
+
+        scratch = calculator.scratch
+
+        scratch_dir = os.path.join(
+            calculator.scratch,
+            "species",
+            conformer.smiles,
+            "conformers")
+
+        f = calc.label + ".log"
+        if not os.path.exists(os.path.join(scratch_dir, f)):
+            logging.info(
+                "It seems that {} was never run...".format(calc.label))
+            result = False
+
+        complete, converged = calculator.verify_output_file(os.path.join(scratch_dir, f))
+
+        if (complete and converged):
+            logging.info(
+            "{} was successful and was validated!".format(calc.label))
+            atoms = self.read_log(
+            os.path.join(scratch_dir, f)
+            )
+
+            starting_molecule = RMGMolecule(SMILES=conformer.smiles)
+            starting_molecule = starting_molecule.toSingleBonds()
+
+            test_molecule = RMGMolecule()
+            test_molecule.fromXYZ(
+            atoms.arrays["numbers"],
+            atoms.arrays["positions"]
+            )
+
+            if not starting_molecule.isIsomorphic(test_molecule):
+                logging.info(
+                "Output geometry of {} is not isomorphic with input geometry".format(calc.label))
+                result = False
+            else:
+                logging.info(
+                "{} was successful and was validated!".format(calc.label))
+                result = True
+
+        if not complete:
+            logging.info(
+                "It appears that {} was killed prematurely".format(calc.label))
+            result = False
+
+        elif not converged:
+            logging.info("{} did not converge".format(calc.label))
+            result = False
+            if isinstance(calculator,Gaussian):
+                if calculator.convergence == "":
+                    logging.info("{} failed QM optimization".format(calc.label))
+                else:
+                    logging.info("resubmitting {} with default convergence criteria".format(calc.label))
+                    atoms = self.read_log(os.path.join(scratch_dir, f))
+                    conformer.ase_molecule = atoms
+                    conformer.update_coords_from("ase")
+                    calc = calculator.get_conformer_calc(
+                        conformer,
+                        convergence=""
+                    )
+                    label = self.submit_conformer(conformer, calc, "general")
+
+                    scratch_dir = os.path.join(
+                    calculator.scratch,
+                    "species",
+                    conformer.smiles,
+                    "conformers"
+                    )
+                    f = calc.label + ".log"
+                    if not os.path.exists(os.path.join(scratch_dir, f)):
+                        logging.info(
+                        "It seems that {} was never run...".format(calc.label))
+                        result = False
+
+                    complete, converged = calculator.verify_output_file(
+                    os.path.join(scratch_dir, f)
+                    )
+
+                    if not complete:
+                        logging.info(
+                        "It appears that {} was killed prematurely".format(calc.label))
+                        result = False
+
+                    elif not converged:
+                        logging.info("{} failed second QM optimization".format(calc.label))
+                        result = False
+
+                    else:
+                        atoms = self.read_log(
+                        os.path.join(scratch_dir, f)
+                        )
+
+                        starting_molecule = RMGMolecule(SMILES=conformer.smiles)
+                        starting_molecule = starting_molecule.toSingleBonds()
+
+                        test_molecule = RMGMolecule()
+                        test_molecule.fromXYZ(
+                        atoms.arrays["numbers"],
+                        atoms.arrays["positions"]
+                        )
+
+                        if not starting_molecule.isIsomorphic(test_molecule):
+                            logging.info(
+                            "Output geometry of {} is not isomorphic with input geometry".format(calc.label))
+                            result = False
+                        else:
+                            logging.info(
+                            "{} was successful and was validated!".format(calc.label))
+                            result = True
+
+        if not result:
+            fail_dir = os.path.join(scratch_dir, "failures")
+            try:
+                os.makedirs(os.path.join(scratch_dir, "failures"))
+            except OSError:
+                logging.info("{} already exists...".format(fail_dir))
+            move(
+                os.path.join(scratch_dir, f),
+                os.path.join(scratch_dir, "failures", f)
+                )
+            return False
+
+        return True
+
+
     def calculate_species(self, species=None, conformer_calculator=None, calculator=None):
 
         logging.info("Calculating geometries for {}".format(species))
@@ -158,137 +290,90 @@ class Job():
         if conformer_calculator:
             species.generate_conformers(calculator=conformer_calculator)
 
-        to_calculate = []
-        results = {}
-        for smiles, conformers in list(species.conformers.items()):
-            results[smiles] = {}
-            for conformer in conformers:
-                calc = calculator.get_conformer_calc(
-                    conformer=conformer
-                )
-                to_calculate.append([conformer, calc])
-
         currently_running = []
-        for conformer, calc in to_calculate:
+        processes = {}
+        for smiles, conformers in list(species.conformers.items()):
 
-            # Pause if you already have 50 jobs, to be more polite to the queue
+            for conformer in conformers:
+
+                process = Process(target=self.calculate_conformer, args=(
+                conformer, calculator))
+                processes[process.name] = process
+
+        for name, process in list(processes.items()):
             while len(currently_running) >= 50:
-                for running_label in currently_running:
-                    if self.check_complete(running_label):
-                        currently_running.remove(running_label)
-                time.sleep(15)
+                for running in currently_running:
+                    if not running.is_alive():
+                        currently_running.remove(name)
+            process.start()
+            # process.join()
+            currently_running.append(name)
 
-            label = self.submit_conformer(conformer, calc, "general")
-            currently_running.append(label)
-
-        # Wait until they're all finished
+        complete = False
         while len(currently_running) > 0:
-            for running_label in currently_running:
-                if self.check_complete(running_label):
-                    currently_running.remove(running_label)
-            logging.info("Waiting for {} conformers to finish.".format(len(currently_running)))
-            time.sleep(15)
+            for name, process in list(processes.items()):
+                if not (name in currently_running):
+                    continue
+                if not process.is_alive():
+                    currently_running.remove(name)
 
-        scratch = calculator.scratch
-        for conformer, calc in to_calculate:
-
-            starting_molecule = RMGMolecule(SMILES=conformer.smiles)
-            starting_molecule = starting_molecule.toSingleBonds()
-
-            scratch_dir = os.path.join(
-                calculator.scratch,
-                "species",
-                conformer.smiles,
-                "conformers"
-            )
-            f = calc.label + ".log"
-            if not os.path.exists(os.path.join(scratch_dir, f)):
-                logging.info(
-                    "It seems that {} was never run...".format(calc.label))
-                result = False
-
-            complete, converged = calculator.verify_output_file(
-                os.path.join(scratch_dir, f)
-            )
-
-            if not complete:
-                logging.info(
-                    "It appears that {} was killed prematurely".format(calc.label))
-                result = False
-
-            elif not converged:
-                logging.info("{} failed QM optimization".format(calc.label))
-                result = False
-
-            else:
-                atoms = self.read_log(
-                    os.path.join(scratch_dir, f)
+        results = []
+        for smiles, conformers in list(species.conformers.items()):
+            for conformer in conformers:
+                scratch_dir = os.path.join(
+                    calculator.scratch,
+                    "species",
+                    conformer.smiles,
+                    "conformers"
                 )
-
-                starting_molecule = RMGMolecule(SMILES=conformer.smiles)
-                starting_molecule = starting_molecule.toSingleBonds()
-
-                test_molecule = RMGMolecule()
-                test_molecule.fromXYZ(
-                    atoms.arrays["numbers"],
-                    atoms.arrays["positions"]
-                )
-
-                if not starting_molecule.isIsomorphic(test_molecule):
+                f = calc.label + ".log"
+                path = os.path.join(scratch_dir, f)
+                if not os.path.exists(os.path.join(scratch_dir, f)):
                     logging.info(
-                        "Output geometry of {} is not isomorphic with input geometry".format(calc.label))
-                    result = False
-                else:
-                    logging.info(
-                        "{} was successful and was validated!".format(calc.label))
-                    result = True
-
-            if not result:
-                fail_dir = os.path.join(scratch_dir, "failures")
+                        "It seems that {} was never run...".format(calc.label))
+                    continue
                 try:
-                    os.makedirs(os.path.join(scratch_dir, "failures"))
-                except OSError:
-                    logging.info("{} already exists...".format(fail_dir))
-                move(
-                    os.path.join(scratch_dir, f),
-                    os.path.join(scratch_dir, "failures", f)
-                )
-            results[conformer.smiles][f] = result
+                    parser = ccread(path)
+                    if parser is None:
+                        logging.info(
+                            "Something went wrong when reading in results for {}...".format(f))
+                        continue
+                    energy = parser.scfenergies[-1]
+                except:
+                    logging.info(
+                        "The parser does not have an scf energies attribute, we are not considering {}".format(f))
+                    energy = 1e5
 
-        for smiles, smiles_results in list(results.items()):
-            scratch_dir = os.path.join(
-                calculator.scratch,
-                "species",
-                conformer.smiles,
-                "conformers"
-            )
+                results.append([energy, conformer, f])
 
-            log_file = os.path.join(scratch_dir, "validations.yaml")
+        results = pd.DataFrame(
+            results, columns=["energy", "conformer", "file"]).sort_values("energy").reset_index()
 
-            with open(log_file, "w") as to_write:
-                yaml.dump(smiles_results, to_write, default_flow_style=False)
-
-            lowest_energy_f = None
-            lowest_energy = 1e5
-
-            for f, result in list(smiles_results.items()):
-                if result:
-                    parser = ccread(os.path.join(scratch_dir, f))
-                    if parser.scfenergies[-1] < lowest_energy:
-                        lowest_energy = parser.scfenergies[-1]
-                        lowest_energy_f = f
-
+        if results.shape[0] == 0:
             logging.info(
-                "The lowest energy conformer is {}".format(lowest_energy_f))
+                "No conformer for {} was successfully calculated... :(".format(reaction))
+            return False
 
-            dest = os.path.join(calculator.scratch,"species",label,lowest_energy_f[:-6] + ".log")
+        for index in range(results.shape[0]):
+            conformer = results.conformer[index]
+            lowest_energy_file = results.file[index]
+            break
 
-            try:
-                copyfile(os.path.join(scratch_dir, lowest_energy_f),dest)
-            except IOError:
-                os.makedirs(os.path.dirname(dest))
-                copyfile(os.path.join(scratch_dir, lowest_energy_f),dest)
+        logging.info(
+            "The lowest energy conformer is {}".format(lowest_energy_file))
 
+        dest = os.path.join(calculator.scratch,"species",conformer,lowest_energy_file + ".log")
+
+        try:
+            copyfile(os.path.join(scratch_dir, lowest_energy_file),dest)
+        except IOError:
+            os.makedirs(os.path.dirname(dest))
+            copyfile(os.path.join(scratch_dir, lowest_energy_file),dest)
+
+        logging.info("The lowest energy file is {}! :)".format(
+            lowest_energy_file))
+
+        return True
 #################################################################################
 
     def submit_transitionstate(self, transitionstate, ase_calculator, partition="general"):
@@ -532,7 +617,7 @@ class Job():
                 results.append([energy, transitionstate, f])
 
         results = pd.DataFrame(
-            results, columns=["energy", "transitionstate", "file"]).sort_values("energy")
+            results, columns=["energy", "transitionstate", "file"]).sort_values("energy").reset_index()
 
         if results.shape[0] == 0:
             logging.info(
