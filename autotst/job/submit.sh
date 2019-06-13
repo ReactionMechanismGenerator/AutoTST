32c0f49ee27e9ba18b924044d0003a06e17a250c
davidfarinajr <farina.d@husky.neu.edu>
2019-06-13 16:05:02 -0400

    added orca modules to submit.sh to run FOD calcs

--- autotst/job/submit.sh
+++ autotst/job/submit.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-$COMMAND "$FILE_PATH.com" > "$FILE_PATH.log"
+if [$COMMAND == "g16"]:
+    $COMMAND "$FILE_PATH.com" > "$FILE_PATH.log"
+if [$COMMAND == 'orca']:
+    module load orca/4.0.1
+    module load gcc/7.2.0
+    module unload openmpi/3.0.2
+    module unload openmpi/3.1.1
+    module unload openmpi/3.1.2
+    module load openmpi/2.0.4
+    /shared/apps/orca/orca_4_0_1_linux_x86-64_openmpi202/orca "$FILE_PATH.inp" > "$FILE_PATH.log"
