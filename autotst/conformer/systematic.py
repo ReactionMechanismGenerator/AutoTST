f11455a7a8590eb8e8739980cdfc0a6d88d0df4a
davidfarinajr <farina.d@husky.neu.edu>
2019-09-11 12:32:51 -0400

    added isomorphism check and conformer copies for rmsd check
    Species will first be optimized without constraints.  If not isomorphic, the original force-field embedded conformer will be used to generate more conformers with fixed bond lengths.

--- autotst/conformer/systematic.py
+++ autotst/conformer/systematic.py
@@ -48,6 +48,8 @@ from autotst.species import Conformer
 from autotst.reaction import TS
 from autotst.conformer.utilities import get_energy, find_terminal_torsions
 
+from rmgpy.molecule import Molecule
+
 from rdkit.Chem import rdMolAlign
 
 
@@ -104,11 +106,11 @@ def find_all_combos(
 
 
 def systematic_search(conformer,
-                      delta = float(120),
+                      delta=float(120),
                       energy_cutoff = 10.0, #kcal/mol
-                      rmsd_cutoff = 1.2, #angstroms
-                      cistrans= True,
-                      chiral_centers= True,
+                      rmsd_cutoff = 1.0, #angstroms
+                      cistrans = True,
+                      chiral_centers = True,
                       multiplicity = True
                       ):
     """
@@ -124,6 +126,98 @@ def systematic_search(conformer,
     - confs (list): a list of unique `Conformer` objects within 1 kcal/mol of the lowest energy conformer determined
     """
     # Takes each of the molecule objects
+
+    reference_mol = conformer.rmg_molecule.copy(deep=True)
+    reference_mol = reference_mol.toSingleBonds()
+    manager = Manager()
+    return_dict = manager.dict()
+
+    def opt_conf(conformer, calculator, i, rmsd_cutoff):
+        """
+        A helper function to optimize the geometry of a conformer.
+        Only for use within this parent function
+        """
+
+        labels = []
+        for bond in conformer.get_bonds():
+            labels.append(bond.atom_indices)
+    
+        if isinstance(conformer, TS):
+            label = conformer.reaction_label
+            ind1 = conformer.rmg_molecule.getLabeledAtom("*1").sortingLabel
+            ind2 = conformer.rmg_molecule.getLabeledAtom("*3").sortingLabel
+            labels.append([ind1, ind2])
+            type = 'ts'
+        else:
+            label = conformer.smiles
+            type = 'species'
+
+        if isinstance(calc, FileIOCalculator):
+            if calculator.directory:
+                directory = calculator.directory 
+            else: 
+                directory = 'conformer_logs'
+            calculator.label = "{}_{}".format(conformer.smiles, i)
+            calculator.directory = os.path.join(directory, label,'{}_{}'.format(conformer.smiles, i))
+            if not os.path.exists(calculator.directory):
+                try:
+                    os.makedirs(calculator.directory)
+                except OSError:
+                    logging.info("An error occured when creating {}".format(calculator.directory))
+
+            calculator.atoms = conformer.ase_molecule
+
+        conformer.ase_molecule.set_calculator(calculator)
+        opt = BFGS(conformer.ase_molecule, logfile=None)
+
+        if type == 'species':
+            if isinstance(i,int):
+                c = FixBondLengths(labels)
+                conformer.ase_molecule.set_constraint(c)
+            try:
+                opt.run(steps=1e6)
+            except RuntimeError:
+                logging.info("Optimization failed...we will use the unconverged geometry")
+                pass
+        
+        if type == 'ts':
+            c = FixBondLengths(labels)
+            conformer.ase_molecule.set_constraint(c)
+            try:
+                opt.run(fmax=0.20, steps=1e6)
+            except RuntimeError:
+                logging.info("Optimization failed...we will use the unconverged geometry")
+                pass
+            
+        conformer.update_coords_from("ase")
+        rmg_mol = Molecule()
+        rmg_mol.fromXYZ(
+            conformer.ase_molecule.arrays["numbers"],
+            conformer.ase_molecule.arrays["positions"]
+        )
+
+        if not rmg_mol.isIsomorphic(reference_mol):
+            logging.info("{}_{} is not isomorphic with reference mol".format(conformer,str(i)))
+            return False
+
+        energy = get_energy(conformer)
+        conformer.energy = energy
+        if len(return_dict)>0:
+            conformer_copy = conformer.copy()
+            for index,conf in return_dict.items():
+                conf_copy = conf.copy()
+                rmsd = rdMolAlign.GetBestRMS(conformer_copy.rdkit_molecule,conf_copy.rdkit_molecule)
+                if rmsd <= rmsd_cutoff:
+                    return True
+        if str(i) != 'ref':
+            return_dict[i] = conformer
+        return True
+
+    if not isinstance(conformer,TS):
+        calc = conformer.ase_molecule.get_calculator()
+        reference_conformer = conformer.copy()
+        if opt_conf(reference_conformer, calc, 'ref', rmsd_cutoff):
+            conformer = reference_conformer
 
     combos = find_all_combos(
         conformer,
@@ -180,75 +274,6 @@ def systematic_search(conformer,
   
         conformers[index] = conformer.copy()
 
-    logging.info(
-        "There are {} unique conformers generated".format(len(conformers)))
-
-    def opt_conf(conformer, calculator, i, rmsd_cutoff):
-        """
-        A helper function to optimize the geometry of a conformer.
-        Only for use within this parent function
-        """
-
-        if isinstance(conformer, TS):
-            labels = []
-            for bond in conformer.get_bonds():
-                labels.append(bond.atom_indices)
-            label = conformer.reaction_label
-            ind1 = conformer.rmg_molecule.getLabeledAtom("*1").sortingLabel
-            ind2 = conformer.rmg_molecule.getLabeledAtom("*3").sortingLabel
-            labels.append([ind1, ind2])
-            type = 'ts'
-        else:
-            label = conformer.smiles
-            type = 'species'
-
-        if isinstance(calc, FileIOCalculator):
-            if calculator.directory:
-                directory = calculator.directory 
-            else: 
-                directory = 'conformer_logs'
-            calculator.label = "{}_{}".format(conformer.smiles, i)
-            calculator.directory = os.path.join(directory, label,'{}_{}'.format(conformer.smiles, i))
-            if not os.path.exists(calculator.directory):
-                try:
-                    os.makedirs(calculator.directory)
-                except OSError:
-                    logging.info("An error occured when creating {}".format(calculator.directory))
-
-            calculator.atoms = conformer.ase_molecule
-
-        conformer.ase_molecule.set_calculator(calculator)
-        opt = BFGS(conformer.ase_molecule, logfile=None)
-
-        if type == 'species':
-            try:
-                opt.run(steps=1e6)
-            except RuntimeError:
-                logging.info("Optimization failed...we will use the unconverged geometry")
-                pass
-        
-        if type == 'ts':
-            c = FixBondLengths(labels)
-            conformer.ase_molecule.set_constraint(c)
-            try:
-                opt.run(fmax=0.20, steps=1e6)
-            except RuntimeError:
-                logging.info("Optimization failed...we will use the unconverged geometry")
-                pass
-            
-        conformer.update_coords_from("ase")
-        energy = get_energy(conformer)
-        conformer.energy = energy
-        if len(return_dict)>0:
-            for index,conf in return_dict.items():
-                rmsd = rdMolAlign.GetBestRMS(conformer.rdkit_molecule,conf.rdkit_molecule)
-                if rmsd <= rmsd_cutoff:
-                    return
-        return_dict[i] = conformer
-        
-
-    manager = Manager()
-    return_dict = manager.dict()
 
     processes = []
     for i, conf in list(conformers.items()):
@@ -272,7 +297,6 @@ def systematic_search(conformer,
 
             process.start()
             active_processes[i] = process
-
     complete = np.zeros_like(active_processes, dtype=bool)
     while not np.all(complete):
         for i, p in enumerate(active_processes):
@@ -288,18 +312,19 @@ def systematic_search(conformer,
             units.eV)].sort_values("energy").reset_index(drop=True)
 
     redundant = []
+    conformer_copies = [conf.copy() for conf in df.conformer]
     for i,j in itertools.combinations(range(len(df.conformer)),2):
-        rmsd = rdMolAlign.GetBestRMS(df.conformer[i].rdkit_molecule,df.conformer[j].rdkit_molecule)
+        copy_1 = conformer_copies[i].rdkit_molecule
+        copy_2 = conformer_copies[j].rdkit_molecule
+        rmsd = rdMolAlign.GetBestRMS(copy_1,copy_2)
         if rmsd <= rmsd_cutoff:
             redundant.append(j)
 
     redundant = list(set(redundant))
     df.drop(df.index[redundant], inplace=True)
-    logging.info("We have identified {} unique conformers for {}".format(
-        len(df.conformer), conformer))
 
     if conformer.rmg_molecule.multiplicity > 2:
-        rads = conformer.rdkit_molecule.getRadicalCount()
+        rads = conformer.rmg_molecule.getRadicalCount()
         if rads % 2 == 0:
             multiplicities = range(1,rads+2,2)
         else:
@@ -321,5 +346,8 @@ def systematic_search(conformer,
             conf.index = i
             confs.append(conf)
             i += 1
+
+    logging.info("We have identified {} unique, low-energy conformers for {}".format(
+        len(confs), conformer))
     
     return confs
