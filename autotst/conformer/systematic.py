c519da1468f2e9757a2066980e088199a4d9fa10
Nate Harms <harms.n@husky.neu.edu>
2020-06-16 13:35:17 -0400

    If a CisTrans is in a ring, we ignore it for the conformer search
    For a CisTrans described by atoms i,j,k,l, we now check to see if j and k are in a ring. If they are, then we don't consider this for a conformer search (i.e. no inversion of these bonds)

--- autotst/conformer/systematic.py
+++ autotst/conformer/systematic.py
@@ -71,7 +71,6 @@ def find_all_combos(
     conformer.get_geometries()
 
     _, torsions = find_terminal_torsions(conformer)
-    cistranss = conformer.cistrans
     chiral_centers = conformer.chiral_centers
 
     torsion_angles = np.arange(0, 360, delta)
@@ -79,7 +78,19 @@ def find_all_combos(
         torsion_angles, repeat=len(torsions)))
 
     if cistrans:
+        cistranss = []
         cistrans_options = ["E", "Z"]
+        try:
+            ring_info = conformer._pseudo_geometry.GetRingInfo()
+        except AttributeError:
+            ring_info = conformer.rdkit_molecule.GetRingInfo()
+
+        for cistrans in conformer.cistrans:
+            i,j,k,l = cistrans.atom_indices
+            if (ring_info.NumAtomRings(i) != 0) or (ring_info.NumAtomRings(k) != 0):
+                continue
+            cistranss.append(cistrans)
+
         cistrans_combos = list(itertools.product(
             cistrans_options, repeat=len(cistranss)))
 
